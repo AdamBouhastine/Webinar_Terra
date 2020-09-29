b525b1b73b2f1f9b5599257bd602c2ea913971ff
AdamBouhastine <bouhastine@et.esiea.fr>
2020-09-29 14:29:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,20 +8,8 @@ terraform {
   }
 }
 
-provider "aws" {
-   profile    = "default"
-   region     = "eu-west-2"
- }
-
-
-resource "aws_instance" "web" {
-  ami                    = "ami-05c424d59413a2876"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
+     resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+   }
