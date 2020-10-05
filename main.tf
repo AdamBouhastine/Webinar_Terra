06435a25db3ee7f8a6d8769c9c59459df2a9d82f
AdamBouhastine <bouhastine@et.esiea.fr>
2020-10-05 14:52:12 +0200

    Changement Infra

--- main.tf
+++ main.tf
@@ -17,11 +17,13 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami                    = "ami-0a669382ea0feb73a"
   instance_type          = "t2.micro"
-     user_data     = <<-EOF
+  security_groups = [aws_security_group.http_access.name]  
+
+    user_data     = <<-EOF
                   #!/bin/bash
                   sudo su
                   yum -y install httpd
-                  echo "<p> Webinar ADN! </p>" >> /var/www/html/index.html
+                  echo "<h1> Webinar ADN </h1> <p> Deploy via GitHub</p>" >> /var/www/html/index.html
                   sudo systemctl enable httpd
                   sudo systemctl start httpd
                   EOF
@@ -30,13 +32,35 @@ resource "aws_instance" "web" {
 	}
 }
 
-resource "aws_security_group" "web-sg" {
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_security_group" "http_access" {
+  name = "Webinar_sg"
+  ingress{
+    from_port         = 80
+    protocol          = "tcp"
+    to_port           = 80
+    cidr_blocks       = ["0.0.0.0/0"]
   }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_db_instance" "my-db-github" {
+  allocated_storage    = 5
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  identifier           = "my-db"
+  username             = "foo"
+  password             = "foobarbaz"
+  skip_final_snapshot = true 
+
 }
 
 
